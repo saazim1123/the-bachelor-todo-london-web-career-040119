91d4eb9fb73753c2b494c1a4e34acf740b1b3c1e
Adnan Azim <saazim1123@gmail.com>
2019-03-23 09:26:10 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,27 @@
 def get_first_name_of_season_winner(data, season)
   # code here
+  data[season].each do |contestant|
+    contestant.each do |k, v|
+      if v == "Winner"
+        name = contestant["name"]
+        return name.split(' ').first
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
   # code here
+  
+  data.each do |season, contestant|
+    contestant.each do |key, value|
+      if value == occupation
+        return contestant["name"]
+        
+      end
+    end
+  end
+
 end
 
 def count_contestants_by_hometown(data, hometown)
