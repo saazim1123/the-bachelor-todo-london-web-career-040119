b851b55bab111ce405b5b7823f149ad4d5afa5bc
Adnan Azim <saazim1123@gmail.com>
2019-03-23 09:26:26 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -14,11 +14,12 @@ def get_contestant_name(data, occupation)
   # code here
   
   data.each do |season, contestant|
-    contestant.each do |key, value|
-      if value == occupation
-        return contestant["name"]
-        
-      end
+    contestant.each do |information|
+     information.each do |key, value|
+       if value == occupation
+         return information["name"]
+       end
+     end
     end
   end
 
@@ -26,12 +27,40 @@ end
 
 def count_contestants_by_hometown(data, hometown)
   # code here
+home = 0
+  data.each do |season, contestant|
+    contestant.each do |information|
+      information.each do |key, value|
+        if value == hometown
+          home += 1
+        end
+      end
+    end
+  end
+home
 end
 
 def get_occupation(data, hometown)
   # code here
+  data.each do |season, contestant|
+    contestant.each do |information|
+      information.each do |key, value|
+        if value == hometown
+          return information["occupation"]
+        end
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
   # code here
+  data[season].each do |information|
+    information.each do |key, value|
+      if key == "age"
+        count += 1 
+        value
+      end
+    end
+  end
 end
